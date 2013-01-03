87c6fc8a3b67fdbe4fe1827315da390945ff2f9c
thomasstorm <thomas_storm@brockmann-consult.de>
2013-01-03 17:19:33 +0100

    significantly generified output

--- src/main/python/Output.py
+++ src/main/python/Output.py
@@ -1,93 +1,140 @@
 from datetime import datetime
+import logging
+from src.main.python import Processor
+from src.main.python.MatchupEngine import MatchupEngine
 
 class Output(object):
 
-    def __init__(self, configuration=None):
-        if configuration is not None:
-            self.configuration = configuration
+    def __init__(self, *args, **kwargs):
+        """Constructs a new instance of Output, tied to a set of statistics.
+        Note that either a 'statistics' or a 'data' object needs to be passed.
 
-    # todo: use **kwargs instead of 10000 parameters
-    def output_basic_statistics(self, variable_name=None, ref_variable_name=None, matchup_count=None, basic_statistics=None, target=None, include_header=None, macro_pixel_size=None, geo_delta=None, time_delta=None, depth_delta=None, source_file=None, separator='\t'):
+        Keyword arguments:
+            statistics -- the statistics to be output (mandatory if data is not given)
+            data -- the data on which to compute statistics to be output (mandatory if statistics is not given)
+            variable_name -- the model variable name (optional; mandatory if statistics is not given)
+            ref_variable_name -- the reference variable name (optional; mandatory if statistics is not given)
+            matchup_count -- the number of matchups (optional)
+            macro_pixel_size -- the macro pixel size used for matchup computation (optional)
+            geo_delta -- the maximum geographical delta used for matchup computation (optional)
+            time_delta -- the maximum time delta used for matchup computation (optional)
+            depth_delta -- the maximum depth delta used for matchup computation (optional)
+            source_file -- a reference to the file the benchmarks were computed on (optional)
+        """
+
+        self.variable_name = kwargs['variable_name'] if 'variable_name' in kwargs.keys() else None
+        self.ref_variable_name = kwargs['ref_variable_name'] if 'ref_variable_name' in kwargs.keys() else None
+        self.matchup_count = kwargs['matchup_count'] if 'matchup_count' in kwargs.keys() else None
+        self.macro_pixel_size = kwargs['macro_pixel_size'] if 'macro_pixel_size' in kwargs.keys() else None
+        self.geo_delta = kwargs['geo_delta'] if 'geo_delta' in kwargs.keys() else None
+        self.time_delta = kwargs['time_delta'] if 'time_delta' in kwargs.keys() else None
+        self.depth_delta = kwargs['depth_delta'] if 'depth_delta' in kwargs.keys() else None
+        self.source_file = kwargs['source_file'] if 'source_file' in kwargs.keys() else None
+
+        if 'statistics' not in kwargs and 'data' not in kwargs:
+            raise ValueError('missing \'statistics\' or \'data\' argument')
+
+        if 'statistics' in kwargs and 'data' in kwargs:
+            logger = logging.getLogger('')
+            logger.setLevel(logging.INFO)
+            logger.warning('both \'statistics\' and \'data\' given: Ignoring \'data\'.')
+
+        self.assign_statistics(kwargs)
+
+    def assign_statistics(self, kwargs):
+        if 'statistics' in kwargs.keys():
+            self.statistics = kwargs['statistics']
+        else:
+            self.data = kwargs['data']
+            if self.variable_name is None or self.ref_variable_name is None:
+                raise ValueError("missing \'variable name\' and/or \'ref_variable_name\' argument(s)")
+            me = MatchupEngine(self.data)
+            matchups = me.find_all_matchups(self.ref_variable_name, self.variable_name, self.macro_pixel_size,
+                self.geo_delta, self.time_delta, self.depth_delta)
+            self.matchup_count = len(matchups)
+            self.statistics = Processor.basic_statistics(matchups=matchups)
+
+    def csv(self, include_header=True, separator='\t', target_file=None):
+        """Outputs the statistics to CSV.
+
+        Keyword arguments:
+            include_header -- if meta information shall be included. Defaults to True
+            separator -- the separator character used in the CSV output. Defaults to '\t'
+            target_file -- if specified, the CSV will be written to the file. Defaults to None
+        """
+
+        lines = []
         if include_header:
-            self.write_header(target, macro_pixel_size, geo_delta, time_delta, depth_delta, source_file)
-        target.write_line(separator.join(header_line_items()))
-        target.write_line(separator.join(data_items(variable_name, ref_variable_name, matchup_count, basic_statistics)))
+            self.write_header(lines)
+        lines.append(separator.join(self.header_line_items()))
+        lines.append(separator.join(self.data_items()))
 
-    def write_header(self, target, macro_pixel_size, geo_delta, time_delta, depth_delta, source_file):
-        source = '' if source_file is None else ' of file \'{}\''.format(source_file)
+        return '\n'.join(lines)
+
+    def write_header(self, lines):
+        source = '' if self.source_file is None else ' of file \'{}\''.format(self.source_file)
         now = datetime.now()
-        target.write_line('##############################################################')
-        target.write_line('#')
-        target.write_line('# Benchmarking results' + source)
-        target.write_line('#')
-        target.write_line('##############################################################')
-        target.write_line('#')
-        target.write_line("# Created on {} {}, {} at {}:{}:{}".format(now.month, now.day, now.year, now.hour, now.minute, now.second))
-        target.write_line("#")
-        target.write_line("# Matchup criteria:")
-        target.write_line("#    Macro pixel size = {}".format(macro_pixel_size))
-        target.write_line("#    Maximum geographic delta = {} \"degrees\"".format(geo_delta))
-        target.write_line("#    Maximum time delta = {} seconds".format(time_delta))
-        if depth_delta is not None:
-            target.write_line("#    Maximum depth delta = {} meters".format(depth_delta))
-        target.write_line("#")
+        lines.append('##############################################################')
+        lines.append('#')
+        lines.append('# Benchmarking results' + source)
+        lines.append('#')
+        lines.append('##############################################################')
+        lines.append('#')
+        lines.append("# Created on {} {}, {} at {}:{}:{}".format(now.month, now.day, now.year, now.hour, now.minute, now.second))
+        lines.append("#")
+        if self.macro_pixel_size is not None or self.geo_delta is not None or self.time_delta is not None or self.depth_delta is not None:
+            lines.append("# Matchup criteria:")
+            lines.append("#    Macro pixel size = {}".format(self.macro_pixel_size))
+            lines.append("#    Maximum geographic delta = {} \"degrees\"".format(self.geo_delta))
+            lines.append("#    Maximum time delta = {} seconds".format(self.time_delta))
+            if self.depth_delta is not None:
+                lines.append("#    Maximum depth delta = {} meters".format(self.depth_delta))
+            lines.append("#")
 
-def header_line_items():
-    header_items = []
-    basic_names = ['min', 'max', 'mean', 'stddev', 'median', 'p90', 'p95']
-    header_items.append('variable_name')
-    header_items.append('ref_variable_name')
-    header_items.append('matchup_count')
-    for bn in basic_names:
-        header_items.append(bn)
-    for bn in basic_names:
-        header_items.append('ref_' + bn)
-    header_items.append('rmsd')
-    header_items.append('unbiased_rmsd')
-    header_items.append('bias')
-    header_items.append('pbias')
-    header_items.append('corrcoeff')
-    header_items.append('reliability_index')
-    header_items.append('model_efficiency')
-    return header_items
-
-
-def data_items(variable_name, ref_variable_name, matchup_count, basic_statistics):
-    data_items = [
-        variable_name,
-        ref_variable_name,
-        str(matchup_count),
-        str(basic_statistics['min']),
-        str(basic_statistics['max']),
-        str(basic_statistics['mean']),
-        str(basic_statistics['stddev']),
-        str(basic_statistics['median']),
-        str(basic_statistics['p90']),
-        str(basic_statistics['p95']),
-        str(basic_statistics['ref_min']),
-        str(basic_statistics['ref_max']),
-        str(basic_statistics['ref_mean']),
-        str(basic_statistics['ref_stddev']),
-        str(basic_statistics['ref_median']),
-        str(basic_statistics['ref_p90']),
-        str(basic_statistics['ref_p95']),
-        str(basic_statistics['rmsd']),
-        str(basic_statistics['unbiased_rmsd']),
-        str(basic_statistics['bias']),
-        str(basic_statistics['pbias']),
-        str(basic_statistics['corrcoeff']),
-        str(basic_statistics['reliability_index']),
-        str(basic_statistics['model_efficiency'])
+    def data_items(self):
+        data_items = [
+            str(self.variable_name),
+            str(self.ref_variable_name),
+            str(self.matchup_count),
+            str('%g' % round(self.statistics['min'], 4)),
+            str('%g' % round(self.statistics['max'], 4)),
+            str('%g' % round(self.statistics['mean'], 4)),
+            str('%g' % round(self.statistics['stddev'], 4)),
+            str('%g' % round(self.statistics['median'], 4)),
+            str('%g' % round(self.statistics['p90'], 4)),
+            str('%g' % round(self.statistics['p95'], 4)),
+            str('%g' % round(self.statistics['ref_min'], 4)),
+            str('%g' % round(self.statistics['ref_max'], 4)),
+            str('%g' % round(self.statistics['ref_mean'], 4)),
+            str('%g' % round(self.statistics['ref_stddev'], 4)),
+            str('%g' % round(self.statistics['ref_median'], 4)),
+            str('%g' % round(self.statistics['ref_p90'], 4)),
+            str('%g' % round(self.statistics['ref_p95'], 4)),
+            str('%g' % round(self.statistics['rmsd'], 4)),
+            str('%g' % round(self.statistics['unbiased_rmsd'], 4)),
+            str('%g' % round(self.statistics['bias'], 4)),
+            str('%g' % round(self.statistics['pbias'], 4)),
+            str('%g' % round(self.statistics['corrcoeff'], 4)),
+            str('%g' % round(self.statistics['reliability_index'], 4)),
+            str('%g' % round(self.statistics['model_efficiency'], 4))
         ]
-    return data_items
+        return data_items
 
-class StringOutputter(object):
-
-    def __init__(self):
-        self.strings = []
-
-    def write_line(self, line):
-        self.strings.append(line)
-
-    def finish(self):
-        return '\n'.join(self.strings)
+    def header_line_items(self):
+        header_items = []
+        basic_names = ['min', 'max', 'mean', 'stddev', 'median', 'p90', 'p95']
+        header_items.append('variable_name')
+        header_items.append('ref_variable_name')
+        header_items.append('matchup_count')
+        for bn in basic_names:
+            header_items.append(bn)
+        for bn in basic_names:
+            header_items.append('ref_' + bn)
+        header_items.append('rmsd')
+        header_items.append('unbiased_rmsd')
+        header_items.append('bias')
+        header_items.append('pbias')
+        header_items.append('corrcoeff')
+        header_items.append('reliability_index')
+        header_items.append('model_efficiency')
+        return header_items
--- src/test/python/OutputTest.py
+++ src/test/python/OutputTest.py
@@ -1,13 +1,11 @@
 from datetime import datetime
 from unittest import TestCase
 from src.main.python import Processor
+from src.main.python.Data import Data
 from src.main.python.Matchup import Matchup
-from src.main.python.Output import Output, StringOutputter
+from src.main.python.Output import Output
 
 class OutputTest(TestCase):
-
-    def setUp(self):
-        self.output = Output()
 
     def test_output_basic_statistics_from_dictionary(self):
 
@@ -19,27 +17,121 @@ class OutputTest(TestCase):
         ]
         stats = Processor.basic_statistics(matchups)
 
-        so = StringOutputter()
+        output = Output(
+            statistics=stats,
+            variable_name='chl',
+            ref_variable_name='chl_ref',
+            matchup_count=4,
+            macro_pixel_size=123,
+            geo_delta=12,
+            time_delta=1234,
+            depth_delta=0.234,
+            source_file='somethingsomethingsomethingdarkside',
+        )
 
-        self.output.output_basic_statistics('chl', 'chl_ref', 4, stats, so, True, 123, 12, 1234, 0.234, 'somethingsomethingsomethingdarkside', '\t')
-
+        expected_result = []
         now = datetime.now()
-        self.assertEqual("##############################################################", so.strings[0])
-        self.assertEqual("#", so.strings[1])
-        self.assertEqual("# Benchmarking results of file \'somethingsomethingsomethingdarkside\'", so.strings[2])
-        self.assertEqual("#", so.strings[3])
-        self.assertEqual("##############################################################", so.strings[4])
-        self.assertEqual("#", so.strings[5])
-        self.assertEqual("# Created on {} {}, {} at {}:{}:{}".format(now.month, now.day, now.year, now.hour, now.minute, now.second), so.strings[6])
-        self.assertEqual("#", so.strings[7])
-        self.assertEqual("# Matchup criteria:", so.strings[8])
-        self.assertEqual("#    Macro pixel size = 123", so.strings[9])
-        self.assertEqual("#    Maximum geographic delta = 12 \"degrees\"", so.strings[10])
-        self.assertEqual("#    Maximum time delta = 1234 seconds", so.strings[11])
-        self.assertEqual("#    Maximum depth delta = 0.234 meters", so.strings[12])
-        self.assertEqual("#", so.strings[13])
-        self.assertEqual("variable_name\tref_variable_name\tmatchup_count\tmin\tmax\tmean\tstddev\tmedian\tp90\tp95\tref_min\tref_max\tref_mean\tref_stddev\tref_median\tref_p90\tref_p95\trmsd\tunbiased_rmsd\tbias\tpbias\tcorrcoeff\treliability_index\tmodel_efficiency", so.strings[14])
-        self.assertEqual("chl\tchl_ref\t4\t9.0\t11.2\t10.425\t0.861321658848\t10.75\t11.14\t11.17\t10.0\t10.0\t10.0\t0.0\t10.0\t10.0\t10.0\t0.960468635615\t0.861321658848\t-0.425\t-4.25\t--\t1.04170735296\tnan", so.strings[15])
+        expected_result.append("##############################################################")
+        expected_result.append("#")
+        expected_result.append("# Benchmarking results of file \'somethingsomethingsomethingdarkside\'")
+        expected_result.append("#")
+        expected_result.append("##############################################################")
+        expected_result.append("#")
+        expected_result.append("# Created on {} {}, {} at {}:{}:{}".format(now.month, now.day, now.year, now.hour, now.minute, now.second))
+        expected_result.append("#")
+        expected_result.append("# Matchup criteria:")
+        expected_result.append("#    Macro pixel size = 123")
+        expected_result.append("#    Maximum geographic delta = 12 \"degrees\"")
+        expected_result.append("#    Maximum time delta = 1234 seconds")
+        expected_result.append("#    Maximum depth delta = 0.234 meters")
+        expected_result.append("#")
+        expected_result.append("variable_name\tref_variable_name\tmatchup_count\tmin\tmax\tmean\tstddev\tmedian\tp90\tp95\tref_min\tref_max\tref_mean\tref_stddev\tref_median\tref_p90\tref_p95\trmsd\tunbiased_rmsd\tbias\tpbias\tcorrcoeff\treliability_index\tmodel_efficiency")
+        expected_result.append("chl\tchl_ref\t4\t9\t11.2\t10.425\t0.8613\t10.75\t11.14\t11.17\t10\t10\t10\t0\t10\t10\t10\t0.9605\t0.8613\t-0.425\t-4.25\tnan\t1.0417\tnan")
+
+        print("\n".join(expected_result))
+
+        print(output.csv())
+
+        self.assertEqual("\n".join(expected_result), output.csv())
+
+    def test_output_basic_statistics_from_dictionary_minimum(self):
+
+        matchups = [
+            Matchup('chl_ref', 'chl', 10, 11, 55, 20, 10000, 0, 0, 0, 0.1, 0),
+            Matchup('chl_ref', 'chl', 10, 9, 55, 20, 10000, 0, 0, 0, 0.1, 0),
+            Matchup('chl_ref', 'chl', 10, 11.2, 55, 20, 10000, 0, 0, 0, 0.1, 0),
+            Matchup('chl_ref', 'chl', 10, 10.5, 55, 20, 10000, 0, 0, 0, 0.1, 0)
+        ]
+        stats = Processor.basic_statistics(matchups)
+
+        output = Output(statistics=stats)
+
+        expected_result = []
+        now = datetime.now()
+        expected_result.append("##############################################################")
+        expected_result.append("#")
+        expected_result.append("# Benchmarking results")
+        expected_result.append("#")
+        expected_result.append("##############################################################")
+        expected_result.append("#")
+        expected_result.append("# Created on {} {}, {} at {}:{}:{}".format(now.month, now.day, now.year, now.hour, now.minute, now.second))
+        expected_result.append("#")
+        expected_result.append("variable_name\tref_variable_name\tmatchup_count\tmin\tmax\tmean\tstddev\tmedian\tp90\tp95\tref_min\tref_max\tref_mean\tref_stddev\tref_median\tref_p90\tref_p95\trmsd\tunbiased_rmsd\tbias\tpbias\tcorrcoeff\treliability_index\tmodel_efficiency")
+        expected_result.append("None\tNone\tNone\t9\t11.2\t10.425\t0.8613\t10.75\t11.14\t11.17\t10\t10\t10\t0\t10\t10\t10\t0.9605\t0.8613\t-0.425\t-4.25\tnan\t1.0417\tnan")
+
+        self.assertEqual("\n".join(expected_result) , output.csv())
+        
+    def test_output_basic_statistics_from_dictionary_no_header(self):
+
+        matchups = [
+            Matchup('chl_ref', 'chl', 10, 11, 55, 20, 10000, 0, 0, 0, 0.1, 0),
+            Matchup('chl_ref', 'chl', 10, 9, 55, 20, 10000, 0, 0, 0, 0.1, 0),
+            Matchup('chl_ref', 'chl', 10, 11.2, 55, 20, 10000, 0, 0, 0, 0.1, 0),
+            Matchup('chl_ref', 'chl', 10, 10.5, 55, 20, 10000, 0, 0, 0, 0.1, 0)
+        ]
+        stats = Processor.basic_statistics(matchups)
+
+        output = Output(statistics=stats)
+
+        expected_result = []
+        expected_result.append("variable_name;ref_variable_name;matchup_count;min;max;mean;stddev;median;p90;p95;ref_min;ref_max;ref_mean;ref_stddev;ref_median;ref_p90;ref_p95;rmsd;unbiased_rmsd;bias;pbias;corrcoeff;reliability_index;model_efficiency")
+        expected_result.append("None;None;None;9;11.2;10.425;0.8613;10.75;11.14;11.17;10;10;10;0;10;10;10;0.9605;0.8613;-0.425;-4.25;nan;1.0417;nan")
+
+        self.assertEqual("\n".join(expected_result) , output.csv(False, ';'))
 
     def test_output_basic_statistics_from_data(self):
-        pass
+        data = Data('../resources/test.nc')
+        output = Output(data=data, variable_name='chl', ref_variable_name='chl_ref')
+
+        expected_result = []
+        now = datetime.now()
+        expected_result.append("##############################################################")
+        expected_result.append("#")
+        expected_result.append("# Benchmarking results")
+        expected_result.append("#")
+        expected_result.append("##############################################################")
+        expected_result.append("#")
+        expected_result.append("# Created on {} {}, {} at {}:{}:{}".format(now.month, now.day, now.year, now.hour, now.minute, now.second))
+        expected_result.append("#")
+        expected_result.append("variable_name\tref_variable_name\tmatchup_count\tmin\tmax\tmean\tstddev\tmedian\tp90\tp95\tref_min\tref_max\tref_mean\tref_stddev\tref_median\tref_p90\tref_p95\trmsd\tunbiased_rmsd\tbias\tpbias\tcorrcoeff\treliability_index\tmodel_efficiency")
+        expected_result.append("chl\tchl_ref\t56\t0.1111\t")
+
+        print(output.csv())
+
+        self.assertTrue(output.csv().startswith("\n".join(expected_result)))
+
+    def test_wrong_initialisation_1(self):
+        with self.assertRaises(ValueError):
+            Output()
+
+    def test_wrong_initialisation_2(self):
+        with self.assertRaises(ValueError):
+            Output(data='dummy_data')
+
+    def test_wrong_initialisation_3(self):
+        with self.assertRaises(ValueError):
+            Output(data='dummy_data', ref_variable_name='ref')
+
+    def test_wrong_initialisation_4(self):
+        with self.assertRaises(ValueError):
+            Output(data='dummy_data', variable_name='var')
