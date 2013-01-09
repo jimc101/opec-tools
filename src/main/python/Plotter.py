b085c2b482ce89ded016d809517052630fb73652
thomasstorm <thomas_storm@brockmann-consult.de>
2013-01-09 11:28:34 +0100

    restructuring; user can choose now if negative correlation shall be included

--- src/main/python/Plotter.py
+++ src/main/python/Plotter.py
@@ -4,17 +4,18 @@ import numpy as np
 import mpl_toolkits.axisartist.floating_axes as FA
 import mpl_toolkits.axisartist.grid_finder as GF
 
-
-def create_taylor_diagram(statistics, target_file=None):
-    diagram = TaylorDiagram(statistics)
+def create_taylor_diagram(ref_stddev, statistics, target_file=None):
+    diagram = TaylorDiagram(ref_stddev)
     figure = pyplot.figure()
 
     diagram.setup_axes(figure)
-    diagram.plot_sample(statistics, 'yh')
+    for stats in statistics:
+        diagram.plot_sample(stats['corrcoeff'], stats['stddev'], 'yh')
 
     if target_file is not None:
         pyplot.savefig(target_file)
 
+    return diagram
 
 def create_target_diagram():
     pass
@@ -28,8 +29,9 @@ class TaylorDiagram(object):
     http://matplotlib.1069221.n5.nabble.com/Taylor-diagram-2nd-take-td28070.html
     """
 
-    def __init__(self, statistics, target_file=None):
-        self.ref_stddev = statistics['ref_stddev']
+    def __init__(self, ref_stddev, show_negative_corrcoeff=True):
+        self.ref_stddev = ref_stddev
+        self.show_negative_corrcoeff = show_negative_corrcoeff
 
     def setup_axes(self, fig):
         """Set up Taylor diagram axes, i.e. single quadrant polar
@@ -39,19 +41,19 @@ class TaylorDiagram(object):
         tr = PolarTransform()
 
         # Intervals on the axis of the correlation coefficient
-        rlocs = np.concatenate((np.arange(10) / 10., [0.95, 0.99]))
+        rlocs = np.concatenate(([-1.0, -0.99, -0.95], np.arange(-10, 0) / 10.0, np.arange(10) / 10.0, [0.95, 0.99]))
 
         # The same intervals as angles
         tlocs = np.arccos(rlocs) # Conversion to polar angles
-
 
         gl1 = GF.FixedLocator(tlocs)    # Positions
         tf1 = GF.DictFormatter(dict(zip(tlocs, map(str, rlocs)))) # maps coefficient angles to string representation of correlation coefficient
 
         y_max = 1.5 * self.ref_stddev # the stddev-axis shall go from 0 to 1.5-times of the stddev
+        x_max = np.pi if self.show_negative_corrcoeff else np.pi / 2
         ghelper = FA.GridHelperCurveLinear(tr,
             extremes=(
-                0, np.pi / 2, # show in grid only the 1st quadrant: from 0 degrees to pi/2 degrees
+                0, x_max, # show in grid only the 1st quadrant: from 0 degrees to pi/2 degrees
                 0, y_max),
             grid_locator1=gl1,
             tick_formatter1=tf1,
@@ -59,6 +61,8 @@ class TaylorDiagram(object):
 
         ax = FA.FloatingSubplot(fig, 111, grid_helper=ghelper) # 111 -> plot contains 1 row, 1 col and shall be located at position 1 (1-based!) in the resulting grid
         fig.add_subplot(ax)
+        if self.show_negative_corrcoeff:
+            fig.text(0.41, 0.178, 'Standard Deviation') # magic numbers: place label central below plot
 
         # Setup axes
         ax.axis["top"].set_axis_direction("bottom")  # "Angle axis"
@@ -67,13 +71,20 @@ class TaylorDiagram(object):
         ax.axis["top"].label.set_axis_direction("top")
         ax.axis["top"].label.set_text("Correlation coefficient")
 
-        ax.axis["left"].set_axis_direction("bottom") # "X axis"
-        ax.axis["left"].label.set_text("Standard deviation")
+        if not self.show_negative_corrcoeff:
+            ax.axis["left"].set_axis_direction("bottom") # "X axis"
+            ax.axis["left"].label.set_text("Standard deviation")
+        else:
+            ax.axis["left"].set_axis_direction("bottom") # "X axis"
 
         ax.axis["right"].set_axis_direction("top")   # "Y axis"
         ax.axis["right"].toggle(ticklabels=True, label=True)
-        ax.axis["right"].major_ticklabels.set_axis_direction("left")
-        ax.axis["right"].label.set_text("Standard deviation")
+        tick_axis_direction = "bottom" if self.show_negative_corrcoeff else "left"
+        ax.axis["right"].major_ticklabels.set_axis_direction(tick_axis_direction)
+        label_axis_direction = "bottom" if self.show_negative_corrcoeff else "left"
+        ax.axis["right"].label.set_axis_direction(label_axis_direction)
+        if not self.show_negative_corrcoeff:
+            ax.axis["right"].label.set_text("Standard deviation")
 
         ax.axis["bottom"].set_visible(False)         # Hide useless axis
 
@@ -92,16 +103,17 @@ class TaylorDiagram(object):
         self.ax.plot([0], self.ref_stddev, 'b*')
 
         # Add stddev contour
-        t = np.linspace(0, np.pi / 2, num=50) # 50 values linearly distributed between 0 and pi/2
+        t = np.linspace(0, x_max, num=50) # 50 values linearly distributed between 0 and pi/2
         r = np.zeros_like(t) + self.ref_stddev # 50 times the stddev
         self.ax.plot(t, r, 'k--', label='_', linewidth=0.5)
 
         # Add rmse contour
         rs, ts = np.meshgrid(np.linspace(0, y_max, num=50),
-            np.linspace(0, np.pi/2, num=50))
+            np.linspace(0, x_max, num=50))
 
         # Unfortunately, I don't understand the next line AT ALL,
         # it's copied from http://matplotlib.1069221.n5.nabble.com/Taylor-diagram-2nd-take-td28070.html
+        # but it leads to the right results (contours of the centered pattern RMS), so I keep it
         rmse = np.sqrt(self.ref_stddev ** 2 + rs ** 2 - 2 * self.ref_stddev * rs * np.cos(ts))
 
         colors = ('#7F0000', '#6F0000', '#5F0000', '#4F0000', '#3F0000', '#2F0000', '#1F0000', '#0F0000')
@@ -111,15 +123,14 @@ class TaylorDiagram(object):
 
         return self.ax
 
-    def get_angle(self, statistics):
-        corr_coeff = statistics['corrcoeff']
-        return np.arccos(corr_coeff)
+    def get_angle(self, corrcoeff):
+        return np.arccos(corrcoeff)
 
-    def plot_sample(self, statistics, *args, **kwargs):
+    def plot_sample(self, corrcoeff, model_stddev, *args, **kwargs):
         """Add model sample to the Taylor diagram. args and kwargs are
         directly propagated to the plot command."""
 
-        theta = self.get_angle(statistics)
-        radius = statistics['stddev']
-        self.ax.plot(theta, radius, *args, **kwargs) # (theta,radius)
+        theta = self.get_angle(corrcoeff)
+        radius = model_stddev
+        self.ax.plot(theta, radius, *args, **kwargs)
 
--- src/test/python/PlotterTest.py
+++ src/test/python/PlotterTest.py
@@ -1,21 +1,29 @@
 import unittest
+#import matplotlib.pyplot as pypl
 import numpy as np
 from src.main.python import Processor
 import src.main.python.Plotter as plotter
 
 class PlotterTest(unittest.TestCase):
 
-    def setUp(self):
-        self.target_file = "../resources/taylor_test.png"
-
     def test_taylor_diagram(self):
-        random = np.random.random(50)
-        values = np.fromfunction(lambda x: np.ones([50]) + random, [50])
-        reference_values = np.fromfunction(lambda x: np.cos(x), [50])
+        values = np.array([0, 15, 2, 3, 15, 8, 5, 3, 9, 11, 12, 1, 7, 7, 6])
+        reference_values = np.array([9, 10, 1, 2, 11, 3, 7, 5, 4, 12, 7, 8, 5, 1, 14])
         stats = Processor.calculate_statistics(model_values=values, reference_values=reference_values)
 
-        plotter.create_taylor_diagram(stats, self.target_file)
+        values1 = np.array([2, 14, 8, 6, 10, 9, 6, 7, 2, 15, 10, 0, 2, 2, 8])
+        reference_values1 = np.array([9, 10, 1, 2, 11, 3, 7, 5, 4, 12, 7, 8, 5, 1, 14])
+        stats1 = Processor.calculate_statistics(model_values=values1, reference_values=reference_values1)
 
-    def tearDown(self):
-        pass
-        #        os.remove(self.targetFile)
+        print('ref_stddev: %s' % stats['ref_stddev'])
+        print('stddev: %s' % stats['stddev'])
+        print('centered rsmd: %s' % stats['centered_rmsd'])
+        print('corrcoeff: %s' % stats['corrcoeff'])
+
+        print('ref_stddev: %s' % stats1['ref_stddev'])
+        print('stddev: %s' % stats1['stddev'])
+        print('centered rsmd: %s' % stats1['centered_rmsd'])
+        print('corrcoeff: %s' % stats1['corrcoeff'])
+
+        plotter.create_taylor_diagram(stats['ref_stddev'], (stats, stats1), '../resources/taylor_test.png')
+        # pypl.show()
