20a0910eae8385ecdfd7c96f121746dae9d23043
thomasstorm <thomas_storm@brockmann-consult.de>
2013-02-01 15:49:30 +0100

    added setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='Opec',
+    version='0.1',
+    description='The Benchmarking Tool of the EU Operational Ecology project, OpEc',
+    author='Thomas Storm',
+    author_email='thomas.storm@brockmann-consult.de',
+    url='http://marine-opec.eu/',
+    packages=['opec'],
+    requires=['numpy (>= 1.6.2)',
+              'scipy (>= 0.11.0)',
+              'netCDF4 (>= 1.0.1)',
+              'nose (>= 1.2.1)',
+              'mako (>= 0.7.3)',
+              'matplotlib (>= 1.2.0)']
+)
